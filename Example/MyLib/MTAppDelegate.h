62fe2f8ea368fe52fd51b1bcb2ea1698982d5a40
dongxuwei <287178790@qq.com>
2017-03-16 17:02:35 +0800

    Initial commit

---
+++ Example/MyLib/MTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MTAppDelegate.h
+//  MyLib
+//
+//  Created by 287178790@qq.com on 03/16/2017.
+//  Copyright (c) 2017 287178790@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
